c2d087e848912c2995a9a4cd669e79709e27020b
Vivien Henry <vivien.henry@outlook.fr>
2019-12-21 14:23:52 +0100

    working dir is in temp

--- stage3-transistor/01-mopidy/00-run.sh
+++ stage3-transistor/01-mopidy/00-run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
 
 on_chroot << EOF
-wget https://github.com/lukh/mopidy/archive/develop.zip
-unzip develop.zip
-pip3 install mopidy-develop/
+wget -P /tmp/ https://github.com/lukh/mopidy/archive/develop.zip
+unzip /tmp/develop.zip -d /tmp/
+pip3 install /tmp/mopidy-develop/
+rm /tmp/develop.zip
+rm -rf /tmp/mopidy-develop
 EOF
 
 install -m 644 files/mopidy.service "${ROOTFS_DIR}/etc/systemd/system/mopidy.service"
