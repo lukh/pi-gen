5b70822be8b69420b022258e42f72fa0d3fe8d2c
Vivien Henry <vivien.henry@outlook.fr>
2019-12-22 19:22:16 +0100

    add mopidy to dialout group

--- stage4-transistor/02-transistor/00-run.sh
+++ stage4-transistor/02-transistor/00-run.sh
@@ -10,4 +10,6 @@ install -m 750 -d "${ROOTFS_DIR}/var/lib/mopidy/transistor/noise_folder"
 on_chroot << EOF
 chown mopidy:audio /var/lib/mopidy/transistor
 chown mopidy:audio /var/lib/mopidy/transistor/noise_folder
+
+usermod -aG dialout mopidy
 EOF
