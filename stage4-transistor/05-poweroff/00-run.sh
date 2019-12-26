faf6d3fae422ee9cedea921f90208e5e55a3ef1d
Vivien Henry <vivien.henry@outlook.fr>
2019-12-26 19:10:41 +0100

    adding sudoers rules for poweroff

--- stage4-transistor/05-poweroff/00-run.sh
+++ stage4-transistor/05-poweroff/00-run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -e
 
-echo "## Admin user group is allowed to execute halt and reboot" >> "${ROOTFS_DIR}/etc/sudoers"
-echo "mopidy ALL=NOPASSWD: /sbin/halt, /sbin/reboot, /sbin/poweroff" >> "${ROOTFS_DIR}/etc/sudoers"
+on_chroot << EOF
+echo "## mopidy user is allowed to execute halt and reboot" >> "/etc/sudoers.d/010_transistor_power_off"
+echo "mopidy ALL=NOPASSWD: /sbin/halt, /sbin/reboot, /sbin/poweroff" >> "/etc/sudoers.d/010_transistor_power_off"
+
+chmod 440 /etc/sudoers.d/010_transistor_power_off
+EOF
